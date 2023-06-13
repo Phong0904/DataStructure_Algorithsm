e01d49072052f32d13c0b473bde29b4361fc5543
Phong0904 <125810967+Phong0904@users.noreply.github.com>
2023-06-13 16:21:59 -0700

    cau16.cpp

---
+++ cau16.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int Max = 50;
+
+void nhap(double a[], int n){
+	for(int i = 0; i < n; i++){
+		cout << "Nhap a[" << i << "]: ";
+		cin >> a[i];  
+	}
+}
+
+void xuat(double a[], int n, int index = 0){
+	if(index >= n){
+		return;
+	}
+	cout << a[index] << " ";
+	xuat(a, n, index + 1);
+}
+
+double sum(double a[], int n, int index = 0){
+	if(index >= n) return 0;
+	double tongduong = sum(a, n, index + 1);
+	if(a[index] > 0){
+		tongduong += a[index];
+	}
+	return tongduong;
+}
+
+int main(){
+	double a[Max]; int n; cin >> n;
+	nhap(a, n); cout << "Mang la: "; xuat(a, n);
+	cout << "Tong duong la: " << sum(a, n) << endl;
+	
+	return 0;
+}
